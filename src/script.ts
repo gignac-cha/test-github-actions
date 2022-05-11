5a8351572829170aefe973308d107bc5c947b015
Jin-Hyeok, Cha <38489680+gignac-cha@users.noreply.github.com>
2022-05-11 18:55:56 +0900

    add initial code

---
+++ src/script.ts
@@ -0,0 +1,5 @@
+window.addEventListener('load', (ev: Event) => {
+    const div: HTMLDivElement = document.createElement('div');
+    div.textContent = 'Hello, world!';
+    document.body.appendChild(div);
+});
